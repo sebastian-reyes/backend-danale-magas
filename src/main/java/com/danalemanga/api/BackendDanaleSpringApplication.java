d3100b58b29bc91533cb43b183673807eeb380b4
Sebastian <sebastianreyes2105@gmail.com>
2022-06-28 18:59:45 -0500

    Creando proyecto, iniciando repo

---
+++ src/main/java/com/danalemanga/api/BackendDanaleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.danalemanga.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDanaleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDanaleSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danalemanga/api/BackendDanaleSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danalemanga.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDanaleSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
